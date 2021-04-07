b78281214e13fbf57a0e9ce8a53534b5d9b1255f
andrescaro189 <andrescaro189@gmail.com>
2021-04-07 13:35:33 -0500

    Se genera la estructura del proyecto en java

---
+++ src/main/java/com/mkyong/reactive/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+
+package com.mkyong.reactive.repository;
+
+import com.mkyong.reactive.model.Comment;
+import reactor.core.publisher.Flux;
+
+public interface CommentRepository {
+
+    Flux<Comment> findAll();
+
+}
